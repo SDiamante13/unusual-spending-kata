7401a05016635d57df501a048e8dc917f099fcbc
Steven Diamante <stevendiamante13@gmail.com>
2022-05-23 23:04:22 -0400

    Passing test for multiple categories.

--- unusual-spending/src/main/java/spending/Detector.java
+++ unusual-spending/src/main/java/spending/Detector.java
@@ -4,34 +4,35 @@ import spending.model.Category;
 import spending.model.Payment;
 import spending.model.UnusualPayment;
 
-import java.util.*;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
+import java.util.Map;
+import java.util.Set;
 
-import static java.util.stream.Collectors.groupingBy;
+import static java.util.stream.Collectors.*;
 
 public class Detector {
 
     public static final double ONE_HUNDRED_FIFTY_PERCENT = 1.5;
 
     public Set<UnusualPayment> detectUnusualPayments(Set<Payment> thisMonthsPayments, Set<Payment> lastMonthsPayments) {
-        int thisMonthPriceForGroceries = getTotalPriceByCategory(thisMonthsPayments);
+        Map<Category, Integer> thisMonthsCategoryToTotalPriceMap = thisMonthsPayments.stream()
+                .collect(groupingBy(Payment::category, summingInt(Payment::price)));
 
-        int lastMonthPriceForGroceries = getTotalPriceByCategory(lastMonthsPayments);
+        Map<Category, Integer> lastMonthsCategoryToTotalPriceMap = lastMonthsPayments.stream()
+                .collect(groupingBy(Payment::category, summingInt(Payment::price)));
 
-        boolean isFiftyPercentMore = thisMonthPriceForGroceries >= lastMonthPriceForGroceries * ONE_HUNDRED_FIFTY_PERCENT;
-        if (isFiftyPercentMore) {
-            return new HashSet<>(Collections.singletonList(
-                    new UnusualPayment(Category.GROCERIES, thisMonthPriceForGroceries)
-            ));
-        }
-        return Collections.emptySet();
+        return thisMonthsCategoryToTotalPriceMap.entrySet()
+                .stream()
+                .filter(thisMonthsEntry ->
+                        isFiftyPercentMore(
+                                thisMonthsEntry.getValue(),
+                                lastMonthsCategoryToTotalPriceMap.get(thisMonthsEntry.getKey())
+                        )
+                )
+                .map(entry -> new UnusualPayment(entry.getKey(), entry.getValue()))
+                .collect(toSet());
     }
 
-    private int getTotalPriceByCategory(Set<Payment> payments) {
-        return payments.stream()
-                .map(Payment::price)
-                .mapToInt(Integer::intValue)
-                .sum();
+    private boolean isFiftyPercentMore(int thisMonthCategoryTotal, int lastMonthCategoryTotal) {
+        return thisMonthCategoryTotal >= lastMonthCategoryTotal * ONE_HUNDRED_FIFTY_PERCENT;
     }
 }
